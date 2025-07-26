3fed79f2eb646ba5df89fc812bba57f83bea1c63
kiktzyyy <ppppbokik@gmail.com>
2025-07-26 17:26:35 +0700

    Create 6.lua

---
+++ bf/6.lua
@@ -0,0 +1,15 @@
+if World3 then
+	ListMasteryMethod = {
+		"Quest",
+		"No Quest",
+		"Nearest",
+		"Cakeprince",
+		"Bones"
+	};
+elseif World2 or World1 then
+	ListMasteryMethod = {
+		"Quest",
+		"No Quest",
+		"Nearest"
+	};
+end;
